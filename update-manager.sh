a4f599e757ecdf091d235c467dbec49f06855a64
estv <esteve.bascunana@epitech.eu>
2026-05-18 15:16:26 +0200

    fix: display formatting

--- update-manager.sh
+++ update-manager.sh
@@ -9,6 +9,7 @@ RESET='\033[0m'
 URL_UPDATE_MANAGER="https://raw.githubusercontent.com/estvv/vps-manager-estv/main/update-manager.sh"
 DEST_UPDATE_MANAGER="$HOME/update-manager.sh"
 
+echo -e "----------------------------------------------"
 echo -e "[1/3] Downloading the latest ${YELLOW}update-manager.sh${RESET} from GitHub..."
 
 if curl -s -f -o "$DEST_UPDATE_MANAGER" "$URL_UPDATE_MANAGER"; then
@@ -17,23 +18,27 @@ else
     echo -e "${RED}Unable${RESET} to download the ${YELLOW}update-manager.sh${RESET}. Check the URL."
     exit 1
 fi
+echo -e "----------------------------------------------\n"
 
 URL_MAKEFILE="https://raw.githubusercontent.com/estvv/vps-manager-estv/main/Makefile.main.template"
 DEST_MAKEFILE="$HOME/Makefile"
 
-echo -e "\n[2/3] Downloading the latest ${YELLOW}Makefile${RESET} from GitHub..."
-
+echo -e "----------------------------------------------"
+echo -e "[2/3] Downloading the latest ${YELLOW}Makefile${RESET} from GitHub..."
 if curl -s -f -o "$DEST_MAKEFILE" "$URL_MAKEFILE"; then
     echo -e "Main ${YELLOW}Makefile${RESET} updated ${GREEN}successfully${RESET} !"
 else
     echo -e "${RED}Unable${RESET} to download the ${YELLOW}Makefile${RESET}. Check the URL."
     exit 1
 fi
+echo -e "----------------------------------------------\n"
 
 URL_SUB_MAKEFILE="https://raw.githubusercontent.com/estvv/vps-manager-estv/main/Makefile.subfolder.template"
 PROJECTS_DIR="$HOME/projects"
 
-echo -e "\n[3/3] Updating ${YELLOW}sub-Makefiles${RESET} in projects...\n"
+echo -e "----------------------------------------------"
+echo -e "[3/3] Updating ${YELLOW}sub-Makefiles${RESET} in projects..."
+echo -e "----------------------------------------------\n"
 
 for DIR in "$PROJECTS_DIR"/*/; do
     if [ -d "$DIR" ]; then
